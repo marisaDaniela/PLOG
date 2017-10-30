1f340f2c74e3ef65c4d69c9b3ac59758b49d1e59
leonormfreitas <leonormendesfreitas@gmail.com>
2017-10-30 18:35:15 +0000

    changes

---
+++ lear.pl
@@ -0,0 +1,48 @@
+:- ensure_loaded('board.pl').
+:- ensure_loaded('menus.pl').
+:- ensure_loaded('utils.pl').
+:- ensure_loaded('gameplay.pl').
+
+:- use_module(library(lists)).
+:- use_module(library(random)).
+:- dynamic state/2.
+:- dynamic state/1.
+
+% displays the game instructions
+
+instructions:-
+		write('+--------- Instructions ---------+'),
+		nl, write('|Black plays first, then turns   |'),
+		nl, write('|alternate. You must choose a    |'),
+		nl, write('|place where want to leave your  |'),
+		nl, write('|piece. First, you have to insert|'),
+		nl, write('|a number between [1-8] to choose|'),
+		nl, write('|the line, and then you choose   |'),
+		nl, write('|the column, choosing again a    |'),
+		nl, write('|number between [1-8]. The goal  |'),
+		nl, write('|of this game is to fill the     |'),
+		nl, write('|board with your mark and dont   |'),
+		nl, write('|let your opponent do the same.  |'),
+		nl, footer,
+		write('|1.Back                   0.Exit|'),
+		nl,repeat,	le(C), C >= 0, C =< 1,
+		analize_five(C).
+		
+hum_hum:-
+	board(Board),
+        player1(B),
+%	player2(W),
+	printBoard(Board),
+	assert(board(Board)),
+	repeat,
+	retract(board(CurrBoard)),
+	makeMove(CurrBoard, NewBoard, B),
+	assert(board(NewBoard)),
+	printBoard(NewBoard).
+
+comp_comp:- write('TODO').
+
+hum_comp:- write('TODO').
+
+
+
